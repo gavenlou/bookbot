a26b927d3061042ca007a0ab3d81df0fe932ce64
gavenlou <gavenlou@gmail.com>
2025-09-30 09:38:25 -0500

    split statistics functions to new file - stats

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
   with open(filepath) as book:
     file_contents = book.read()
     return file_contents
 
-def number_of_words(text):
-  print(f"Found {len(text.split())} total words")
-
 def main():
   book = get_book_text('books/frankenstein.txt')
-  number_of_words(book)
+  get_num_words(book)
 
 main()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def get_num_words(text):
+  print(f"Found {len(text.split())} total words")
